e33639a2ba43f328329b888ed87c926bf3d268f9
Joyce3700 <82375545+Joyce3700@users.noreply.github.com>
2021-04-12 18:08:58 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
 
+i <- NULL
+y <- function(n) {
+z <<- n
+i <<- NULL
 }
 
 
-## Write a short comment describing this function
+## I changed the "sInverse" and "putInverse"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+put <- function() z
+yInverse <- function(inverse) i <<- inverse
+placeInverse <- function() i
+list(set = set,
+get = get,
+yInverse = sInverse,
+placeInverse = placeInverse)
 }
